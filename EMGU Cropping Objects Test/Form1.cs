8f1491071a466376aa16a8ed410c228f7b66187e
Ibnu Ahsani <ibnu.muhari@gmail.com>
2019-06-02 15:46:11 +0700

    feat: find the input image's connected components

--- EMGU Cropping Objects Test/Form1.cs
+++ EMGU Cropping Objects Test/Form1.cs
@@ -33,5 +33,38 @@ namespace EMGU_Cropping_Objects_Test
                 leftPictureBox.Image = imageInput.Bitmap;
             }
         }
+
+        private void processToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (imageInput == null)
+            {
+                return;
+            }
+
+            try
+            {
+                //  Convert img to grayscale -> convert img to binary using max threshold of 100
+                //  (if value is more than 100, set it to the max value [255]) -> close effects
+                //  using dilation then erosion
+                var temp = imageInput.Convert<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(255))
+                            .Dilate(1).Erode(1);
+
+                // Matrix to store the labels from the image
+                Mat labels = new Mat();
+
+                // Find the number of connected components
+                int nLabels = CvInvoke.ConnectedComponents(temp, labels);
+
+                //  Konvesi labels menjadi sebuah image
+                connectedComponents = labels.ToImage<Gray, byte>();
+
+                // connectedComponents can not be shown because it is a value between 1 - 0
+                rightPictureBox.Image = temp.Bitmap;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
     }
 }
